007eed0812f77aeb815475e910e085d6c0e54ef5
EduardoAngelo1 <eduardoangelo300@gmail.com>
2023-11-05 21:18:43 -0300

    [ADD] função favoritar

--- src/components/CardProduto.jsx
+++ src/components/CardProduto.jsx
@@ -1,32 +1,26 @@
 import React from 'react'
-import { SiGithubsponsors } from "react-icons/si";
 import { useNavigate } from 'react-router-dom';
 
 const CardProduto = ({ produto }) => {
-
+    
     const navigate = useNavigate()
 
     const handleChangeRouter = (produto) => {
 
         navigate(`/produtos/${produto.id}`)
     }
-
+    
     return (
 
         <div onClick={() => { handleChangeRouter(produto) }} className='item'>
 
-            <img src={produto.urlimg} alt="foto-produto" />
+            <img className='teste2' src={produto.urlimg} alt="foto-produto" />
 
             <div id="quadro">
                 <span>{produto.nome}</span> <br />
                 <h4>R${produto.preco}</h4>
             </div>
-
-            <i>
-                <SiGithubsponsors />
-            </i>
-
-        </div>
+        </div>   
     )
 }
 
--- src/pages/Pedidos.jsx
+++ src/pages/Pedidos.jsx
@@ -1,32 +1,65 @@
+
 import { api } from '../api/Api'
 import { Navbar } from '../components/Navbar'
 
 import styled from 'styled-components'
 
 const StyledDiv = styled.div`
-
-    width: 100vw;
-    height: 100vh;
-    display: flex;
+  /* width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content:center; */
+    display: flex; 
+    flex-direction: column; 
+    justify-content: center; 
     align-items: center;
-    justify-content: center;
+    height: 100vh; 
 
-    h1 {
-        font-size: 10em; 
+
+  .campo-pedido {
+    width: 1000px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    border: 5px solid #ccc;
+    box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+
+    .nome-produto {
+      font-size: 18px;
+      font-weight: bold;
+      color: #333;
     }
-    
-`
+
+    .quantidade-produto {
+      font-size: 16px;
+      color: #555;
+    }
+
+    .numero-pedido {
+      font-size: 16px;
+      color: #000;
+    }
+  }
+`;
 
 const Pedidos = () => {
     return (
-
         <>
             <Navbar />
             <StyledDiv>
-                <h1>Pedidos</h1> 
+                <h1>Pedidos</h1>
+                <br />
+                <div className="campo-pedido">
+                    <p className="nome-produto">Produto 1</p>
+                    <p className="quantidade-produto">Quantidade: 1</p>
+                    <p className="numero-pedido">Número do pedido: 123</p>
+                </div>
+                <button>Confirmar Pedido</button>
             </StyledDiv>
         </>
-    )
-}
+    );
+};
 
-export default Pedidos
+export default Pedidos;
